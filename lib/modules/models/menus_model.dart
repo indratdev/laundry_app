e70ffbcbd141e754f0e116ffa66fa4e090d4154e
indratdev <indrat.dev@gmail.com>
2022-05-31 17:12:36 +0700

    updated

--- lib/modules/models/menus_model.dart
+++ lib/modules/models/menus_model.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:laundry_app/config/routes/routes.dart';
 
 class MenusModel {
   String name, title;
-  Routes route;
-  IconData icon;
+  String route;
+  Icon iconn;
 
   MenusModel({
     required this.name,
     required this.title,
     required this.route,
-    required this.icon,
+    required this.iconn,
   });
+
+  MenusModel getMainMenu() {
+    return MenusModel(
+        name: 'Transaksi',
+        title: 'Transaksi',
+        route: '/transaksi',
+        iconn: Icon(Icons.menu));
+  }
 }
